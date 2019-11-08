a7126ad5ccb836b4323638260960e4ea84a7dc70
chenchenghao <chenghao.chen@ximalaya.com>
2019-11-08 20:40:32 +0800

    实现 增强类重置功能。

--- demo2/src/main/java/com/cch/agentdemo/demo2/MyAgentmain.java
+++ demo2/src/main/java/com/cch/agentdemo/demo2/MyAgentmain.java
@@ -7,21 +7,51 @@ package com.cch.agentdemo.demo2;
  */
 import com.cch.agentdemo.core.MyTransformer;
 
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
+import java.security.ProtectionDomain;
 
 public class MyAgentmain {
 
     public static void agentmain(String agentOps, Instrumentation inst) throws UnmodifiableClassException {
         String[] args = agentOps.split("&");
-
+        MyTransformer myTransformer =null;
+        Class temp =null;
         for(Class clazz : inst.getAllLoadedClasses()) {
             if(clazz.getName().equals(args[0])) {
                 // 绑定ClassFileTransformer
-                inst.addTransformer(new MyTransformer(args[0], args[1]), true);
+                myTransformer = new MyTransformer(args[0], args[1]);
+                inst.addTransformer(myTransformer, true);
                 // 对已加载类重新转换处理
+                temp =clazz;
                 inst.retransformClasses(clazz);
+
             }
+        }
+        inst.removeTransformer(myTransformer);
+        try {
+
+            Thread.sleep(5000);
+            System.out.println("reset-----------");
+            final ClassFileTransformer resetClassFileTransformer = new ClassFileTransformer() {
+                @Override
+                public byte[] transform(
+                        ClassLoader loader,
+                        String className,
+                        Class<?> classBeingRedefined,
+                        ProtectionDomain protectionDomain,
+                        byte[] classfileBuffer) throws IllegalClassFormatException {
+                    return null;
+                }
+            };
+
+            inst.addTransformer(resetClassFileTransformer,true);
+            inst.retransformClasses(temp);
+            inst.removeTransformer(resetClassFileTransformer);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 
